f366fb59bf8558063bcf22712747e9f909f8b334
Martin Nusspaumer <142355408+KlassicStudio@users.noreply.github.com>
2023-10-25 23:31:04 +0000

    pause - resume btn added

--- src/js/component/counter.jsx
+++ src/js/component/counter.jsx
@@ -2,31 +2,46 @@ import React, { useState, useEffect } from 'react';
 
 function SecondsCounter() {
   const [seconds, setSeconds] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds((prevSeconds) => prevSeconds + 1);
-    }, 1000);
+    let intervalId;
 
-    return () => clearInterval(interval);
-  }, []);
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setSeconds((prevSeconds) => prevSeconds + 1);
+      }, 1000);
+    } else {
+      clearInterval(intervalId);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isRunning]);
 
   const resetCounter = () => {
     setSeconds(0);
   };
 
+  const toggleRunning = () => {
+    setIsRunning(!isRunning);
+  };
+
   const timeString = String(seconds).padStart(6, '0');
 
   return (
-    <div className='container-fluid d-flex justify-content-center bg-dark align-items-center vh-100'>
+  <div className='row container-fluid d-flex justify-content-center bg-dark align-items-center vh-100'>
+   <div>
       <div id="counter">
-        <i class="fa-solid fa-clock"></i>{timeString}
+      {isRunning ? <i class="fa-regular fa-clock"></i> : <i class="fa-solid fa-clock"></i>}{timeString}
       </div>
       <div>
       <button onClick={resetCounter}>Reset</button>
+      <button onClick={toggleRunning}>
+        {isRunning ? 'Pause' : 'Resume'}
+      </button>
       </div>      
-      
-    </div>
+    </div> 
+  </div>
   );
 }
 
